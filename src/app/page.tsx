0354fd2e2fe355855d5f4ffe3a877f01c76b3413
sabinabale <104379027+sabinabale@users.noreply.github.com>
2024-07-31 12:22:26 +0200

    login ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,52 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/Button";
+import Image from "next/image";
+import loginpic from "/public/login.png";
 
 export default function Page() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <h1>Company</h1>
-      <form className="mt-4 flex flex-col">
-        <h2>Log into your account</h2>
-        <p>Welcome back! Please enter your details.</p>
-        <label htmlFor="email">Email:</label>
+      <h1 className="text-[22px] font-bold mb-5">Company</h1>
+      <form className="relative flex flex-col w-[360px] h-auto border rounded-[15px] p-5 bg-white">
+        <Image
+          src={loginpic.src}
+          width={360}
+          height={92}
+          alt="login backgroubd picture"
+          className="absolute top-0 left-0"
+        ></Image>
+        <h4 className="text-center mt-[34px]">Log into your account</h4>
+        <p className=" text-zinc-500 text-center mb-11 mt-2">
+          Welcome back! Please enter your details.
+        </p>
+        <label htmlFor="email">E-mail</label>
         <input
           type="text"
           id="email"
           name="email"
-          placeholder="name@email.com"
+          placeholder="Enter email address"
+          className="input"
+          required
         />
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password</label>
         <input
           type="password"
           id="password"
           name="password"
-          placeholder="Super secret password"
+          placeholder="Enter password"
+          required
+          className="input"
         />
         <div className="flex gap-4 mt-2">
-          <Button variant="primary" type="submit">
-            Log in
-          </Button>
-          <Button variant="secondary" type="submit">
-            <Link href="/signup">Sign up</Link>
+          <Button variant="primary" type="submit" className="w-full">
+            <Link href="/app/clients">Log in</Link>
           </Button>
         </div>
       </form>
+      <small className="mt-4 text-zinc-400">
+        All rights reserved © {new Date().getFullYear()}
+      </small>
     </div>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,11 +3,11 @@ import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-lg font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-opacity-85",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-[14px] font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-opacity-85",
   {
     variants: {
       variant: {
-        primary: "bg-[#FFB100] text-[#292524]",
+        primary: "bg-[#FFB100] text-[#292524] py-2 px-4",
         destructive: "bg-red-500 hover:bg-red-500/90",
         secondary: "bg-zinc-300 text-zinc-600",
         outline:
